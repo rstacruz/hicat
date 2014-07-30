f1186972213ab2fe8288d6283dec9b16cf593c82
Rico Sta. Cruz <hi@ricostacruz.com>
2014-07-31 07:46:15 +0800

    Refactors colors. Adds support for Ruby string interpolation. *

--- index.js
+++ index.js
@@ -41,61 +41,7 @@ function Hicat (str, options) {
  * The color scheme. You can modify the color scheme if you wish.
  */
 
-Hicat.colors = {
-  // aliases
-  GREY: '30',
-  RED: '31',
-  GREEN: '32',
-  YELLOW: '33',
-  BLUE: '34',
-  MAGENTA: '35',
-  BOLD: '1',
-  BOLDWHITE: '1;37',
-
-  keyword: 'BOLD',
-  built_in: 'keyword',
-  tag: 'keyword', /* css tag */
-  class: 'keyword', /* css class */
-
-  title: '4', /* tags, function names */
-
-  comment: 'GREY',
-  doctype: 'comment',
-  pi: 'comment', /* xml declaration */
-  preprocessor: 'comment', /* C/C++ preprocessor directives */
-  horizontal_rule: 'comment',  /* markdown HR --- */
-
-  string: 'GREEN',
-  value: 'string', /* html/json values */
-  code: 'string', /* markdown code */
-  link_reference: 'string', /* markdown link reference */
-
-  number: 'YELLOW',
-  symbol: 'number', /* ruby :symbols */
-  hexcolor: 'number', /* css hex holor */
-  link_url: 'number', /* markdown link url */
-
-  'function': 'BOLD', /* css function, like rgba() */
-  strong: 'function', /* markdown strong */
-  link_label: 'strong', /* markdown links */
-
-  header: 'BOLDWHITE', /* markdown header */
-
-  attribute: 'BLUE', /* html/json attributes */
-  literal: 'attribute', /* true, false, etc */
-  variable: 'attribute', /* xml variable */
-
-  pseudo: 'GREY', /* css pseudoclass :before */
-  attr_selector: 'pseudo', /* css attr [type='number'] */
-  blockquote: 'pseudo', /* markdown blockquote */
-
-  params: '0', /* function parameters () */
-  regexp: 'MAGENTA',
-
-  addition: 'GREEN', /* diff + */
-  deletion: 'RED',   /* diff - */
-  chunk: 'BLUE',     /* diff @@ -1,119 +1,768 @@ */
-};
+Hicat.colors = require('./lib/colors');
 
 /**
  * Hicat.listLanguages():
@@ -145,17 +91,26 @@ var color = Hicat.color = function (token) {
  */
 
 function html2ansi (str) {
+  // do multiple passes as spans can be multiple
+  while (~str.indexOf('<span class="hljs-')) {
+    str = replaceSpan(str);
+  }
+
   return str
-    .replace(/<span class="hljs-([^"]*)">([^<]*)<\/span>/g, function (_, token, s) {
-      var code = color(token);
-      if (process.env.HICAT_DEBUG) s = s + "\033[0;30m[" + token + "]\033[0m";
-      return colorize(s, code);
-    })
     .replace(/<span class="([^"]*)">/g, '')
     .replace(/<\/span>/g, '')
     .replace(/&lt;/g, '<')
     .replace(/&gt;/g, '>')
     .replace(/&amp;/g, '&');
+}
+
+function replaceSpan (str) {
+  return str
+    .replace(/<span class="hljs-([^"]*)">([^<]*)<\/span>/g, function (_, token, s) {
+      var code = color(token);
+      if (process.env.HICAT_DEBUG) s = s + "\033[0;30m[" + token + "]\033[0m";
+      return colorize(s, code);
+    });
 }
 
 /**
@@ -172,7 +127,14 @@ function colorize (s, color) {
   var reset = "\033[0m",
       code = "\033[" + color + "m";
 
+  // improved support for less -R, since it automatically resets the colors per line
   if (~s.indexOf("\n")) s = s.replace(/\n/g, "\n"+code);
+
+  // nesting
+  if (~s.indexOf(reset)) {
+    s = s.replace(/\033\[0m/g, code);
+  }
+
   return code + s + reset;
 }
 
